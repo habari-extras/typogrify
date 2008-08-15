9a2fe82a32c5abd81ac8f4839e6c7a81f2685d87
chrismeller <chris@doesnthaveone.com>
2008-08-15 06:51:49 +0000

    Importing Typogrify plugin.
    git-svn-id: http://svn.habariproject.org/habari-extras/plugins/typogrify/trunk@752 f4a90942-1786-4aa1-b092-57c31935ddc3

---
+++ typogrify.plugin.php
@@ -0,0 +1,194 @@
+<?php
+
+	class Typogrify extends Plugin {
+		
+		const VERSION = '0.1';
+		
+		public function info ( ) {
+			
+			return array(
+				'name' => 'Typogrify',
+				'url' => 'http://habariproject.org',
+				'author' => 'Habari Community',
+				'authorurl' => 'http://habariproject.org',
+				'version' => self::VERSION,
+				'description' => '',
+				'license' => 'Apache License 2.0',
+			);
+			
+		}
+		
+		public function action_update_check ( ) {
+			
+			Update::add( 'Typogrify', '757310B4-6A86-11DD-A4F6-207155D89593', $this->info->version );
+			
+		}
+		
+		public function filter_plugin_config ( $actions, $plugin_id ) {
+			
+			if ( $plugin_id == $this->plugin_id() ) {
+				$actions[] = _t( 'Configure' );
+			}
+			
+			return $actions;
+			
+		}
+		
+		public function action_plugin_ui ( $plugin_id, $action ) {
+			
+			if ( $plugin_id == $this->plugin_id() ) {
+				
+				if ( $action == _t( 'Configure' ) ) {
+					
+					$class_name = strtolower( get_class( $this ) );
+					
+					$form = new FormUI( $class_name );
+					
+					$form->append( 'fieldset', 'standard_options', 'Standard Options' );
+					$form->standard_options->append( 'checkbox', 'do_amp', 'typogrify__do_amp', _t( 'Add <code>&lt;span class=&quot;amp&quot;&gt;</code> to ampersands.') );
+					$form->standard_options->append( 'checkbox', 'do_widont', 'typogrify__do_widont', _t( 'Try to prevent <a href="http://en.wikipedia.org/wiki/Widows_and_orphans">widows</a> by adding <code>&amp;nbsp;</code> between the last two words in blocks of text.' ) );
+					$form->standard_options->append( 'checkbox', 'do_smartypants', 'typogrify__do_smartypants', _t( 'Apply <a href="http://michelf.com/projects/php-smartypants/">SmartyPants</a> to text.' ) );
+					$form->standard_options->append( 'checkbox', 'do_caps', 'typogrify__do_caps', _t( 'Add <code>&lt;span class=&quot;caps&quot;&gt;</code> to consecutive capital letters (acronyms, etc.).' ) );
+					$form->standard_options->append( 'checkbox', 'do_initial_quotes', 'typogrify__do_initial_quotes', _t( 'Add <code>&lt;span class=&quot;dquo&quot;&gt;</code> to initial double quotes, and <code>&lt;span class=&quot;quo&quot;&gt;</code> to initial single quotes.' ) );
+					
+					$form->append( 'fieldset', 'special_options', 'Special Options' );
+					$form->special_options->append( 'checkbox', 'do_guillements', 'typogrify__do_guillements', _t( 'Add <code>&lt;span class=&quot;dquo&quot;&gt;</code> to initial <a href="http://en.wikipedia.org/wiki/Guillemet">Guillemets</a> (&laquo; or &raquo;) as well.' ) );
+					$form->special_options->append( 'checkbox', 'do_dash', 'typogrify__do_dash', _t( 'Add thin spaces (<code>&amp;thinsp;</code>) to both sides of em and en dashes.' ) );
+					
+					$form->append( 'submit', 'save', _t( 'Save' ) );
+					
+					$form->on_success( array( $this, 'updated_config' ) );
+					$form->out();
+					
+				}
+				
+			}
+			
+		}
+		
+		public function updated_config ( $form ) {
+			
+			$form->save();
+			
+		}
+		
+		public function action_plugin_activation ( $file ) {
+			
+			if ( Plugins::id_from_file( $file ) == Plugins::id_from_file( __FILE__ ) ) {
+				
+				// default options: name => default value
+				$options[ 'typogrify__do_amp' ] = 1;
+				$options[ 'typogrify__do_widont' ] = 1;
+				$options[ 'typogrify__do_smartypants' ] = 1;
+				$options[ 'typogrify__do_caps' ] = 1;
+				$options[ 'typogrify__do_initial_quotes' ] = 1;
+				$options[ 'typogrify__do_guillements' ] = 1;
+				$options[ 'typogrify__do_dash' ] = 1;
+				
+				foreach ( $options as $option => $value ) {
+					
+					if ( Options::get( $option ) == null ) {
+						Options::set( $option, $value );
+					}
+					
+				}
+				
+			}
+			
+		}
+		
+		public function filter ( $text ) {
+			
+			if ( Options::get( 'typogrify__do_amp' ) ) {
+				$text = amp( $text );
+			}
+			
+			if ( Options::get( 'typogrify__do_widont' ) ) {
+				$text = widont( $text );
+			}
+			
+			if ( Options::get( 'typogrify__do_smartypants' ) ) {
+				$text = SmartyPants( $text );
+			}
+			
+			if ( Options::get( 'typogrify__do_caps' ) ) {
+				$text = caps( $text );
+			}
+			
+			if ( Options::get( 'typogrify__do_initial_quotes' ) ) {
+				$text = initial_quotes( $text );
+			}
+			
+			if ( Options::get( 'typogrify__do_guillemets' ) ) {
+				$text = initial_quotes( $text, true );
+			}
+			
+			if ( Options::get( 'typogrify__do_dash' ) ) {
+				$text = dash( $text );
+			}
+			
+			return $text;
+			
+		}
+		
+		public function filter_post_title_out ( $title ) {
+			
+			return $this->filter( $title );
+			
+		}
+		
+		public function filter_post_content_out ( $content ) {
+			
+			return $this->filter( $content );
+			
+		}
+		
+		public function filter_post_excerpt_out ( $excerpt ) {
+			
+			return $this->filter( $excerpt );
+			
+		}
+		
+		public function filter_comment_content_out ( $comment ) {
+			
+			return $this->filter( $comment );
+			
+		}
+		
+		public function filter_comment_name_out ( $name ) {
+			
+			return $this->filter( $name );
+			
+		}
+		
+		public function filter_post_tags_out ( $tags ) {
+			
+			return $this->filter( $tags );
+			
+		}
+		
+		/**
+		 * Set all our filters to run after everything else did
+		 */
+		public function set_priorities ( ) {
+			
+			return array(
+				'filter_post_title_out' => 10,
+				'filter_post_content_out' => 10,
+				'filter_post_excerpt_out' => 10,
+				'filter_comment_content_out' => 10,
+				'filter_comment_name_out' => 10,
+				'filter_post_tags_out' => 10,
+			);
+			
+		}
+		
+		public function action_init ( ) {
+			
+			include( 'php-typogrify.php' );
+			
+		}
+		
+	}
+
+?>
